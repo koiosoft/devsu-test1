d70d3506cde82038bba63e0c584f05a011d68287
Roger Zavala <roger.zavala@koiosoft.com>
2023-09-28 18:19:49 -0400

    - I was implementing the search

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -13,9 +13,11 @@ export class ListComponent implements OnInit {
   testImageUrl =
     'https://www.visa.com.co/dam/VCOM/regional/lac/SPA/Default/Pay%20With%20Visa/Tarjetas/visa-classic-credito-centrada-400x225.jpg';
 
+  private source: IProduct[] = [];
   _products: IProduct[] = [];
   loading: boolean = true;
-  currentPage: number = 1;
+  currentPage: number = 0;
+  search: string = '';
 
   constructor(private productService: ProductService) {}
 
@@ -24,6 +26,7 @@ export class ListComponent implements OnInit {
       if (data) {
         this.loading = false;
         this._products = data;
+        this.source = data;
       }
     });
   }
@@ -48,5 +51,20 @@ export class ListComponent implements OnInit {
 
   onChangePage(event: Event) {
     this.currentPage = Number((event.target as HTMLSelectElement).value);
+  }
+
+  onSearch(event: Event) {
+    const search: string = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+    if (search !== '') {
+      this._products = this.source.filter(
+        (p) =>
+          p.name.toLowerCase().indexOf(search) > -1 ||
+          p.description.toLowerCase().indexOf(search) > -1
+      );
+    } else {
+      this._products = this.source;
+    }
   }
 }
